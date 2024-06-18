a383246122e34bf029c8010d7d9a068373a6d69d
Alex <81379611+halexio@users.noreply.github.com>
2024-06-18 12:16:40 +0100

    Update agent_unix.go

--- agent/agent_unix.go
+++ agent/agent_unix.go
@@ -27,7 +27,7 @@ import (
 	"syscall"
 	"time"
 
-	rmm "github.com/amidaware/rmmagent/shared"
+	rmm "github.com/halexio/rmmagent/shared"
 	ps "github.com/elastic/go-sysinfo"
 	"github.com/go-resty/resty/v2"
 	"github.com/jaypipes/ghw"
