e23cfe3f4bcaf049d181a0e8abf9cbf345016181
Alex <81379611+halexio@users.noreply.github.com>
2024-06-18 12:20:52 +0100

    Update wmi_windows.go

--- agent/wmi_windows.go
+++ agent/wmi_windows.go
@@ -15,7 +15,7 @@ import (
 	"encoding/json"
 
 	"github.com/StackExchange/wmi"
-	rmm "github.com/amidaware/rmmagent/shared"
+	rmm "github.com/halexio/rmmagent/shared"
 )
 
 func GetWin32_USBController() ([]interface{}, error) {
